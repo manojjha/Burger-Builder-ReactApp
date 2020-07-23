644dc9af6413fdd10bee3e8eba16fde9264500e8
manoj jha <manojjha491@live.com>
2020-07-23 15:43:13 +0530

    just push all

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
-        <div>ToolBar, SideWar, BackDrop </div>
+        {/* <div>ToolBar, SideWar, BackDrop </div> */}
         <main className={classes.Content}>
             {props.children}
         </main>
